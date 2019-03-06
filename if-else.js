b449853774deb925ec0c2e726faeb3f736882d3f
Andri Tri Setiawan <andritri86@gmail.com>
2019-03-06 12:33:00 +0700

    latihan JS

---
+++ if-else.js
@@ -0,0 +1,17 @@
+function jalanSatu() {
+  var nilai = 1;
+  if (nilai === 3){
+    console.log("Benar")
+  }else if(nilai === 2){
+    console.log("DUA")
+  }
+  else{
+      console.log("Salah")
+  }
+
+  //cara 2 if else
+    // if(nilai === 2)console.log("OK")
+    //   else console.log("BUKAN")
+}
+
+jalanSatu()
---
+++ index.js
@@ -0,0 +1,9 @@
+var message = "Hello Coding";
+let msg1 = "Hello";
+
+const msg2 = "Coding"
+
+
+msg1 = "rubah3"
+
+console.log(msg1);
